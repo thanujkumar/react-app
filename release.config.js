f108a633ce27cb824f253fb43b956cf03c785d25
THANUJ KUMAR S C <thanuj_tk@yahoo.com>
2021-06-30 18:06:48 +0530

    fix: Feature/fix5 (#21)
    * fix: renamed branches to have correct default (#17)
    
    * feat: format and more info in docs (#18)
    
    * fix: url issue in release.config.js

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@
 //https://github.com/semantic-release/semantic-release/blob/master/docs/usage/configuration.md#configuration
 module.exports = {
   branches: "master",
-  repositoryUrl: "https://github.com/thanujkumar/react-app/",
+  repositoryUrl: "https://github.com/thanujkumar/react-app",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
