f633c2e6ec8e7aae8a08884d559b46e1bc3bc025
THANUJ KUMAR S C <thanuj_tk@yahoo.com>
2021-07-29 12:59:52 +0530

    format fix

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ import workflow from "./github-workflow.jpg";
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <img src={workflow} alt="workflow"/>
-                <p>
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React with thanujtk
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={workflow} alt="workflow" />
+        <p></p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React with thanujtk
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
